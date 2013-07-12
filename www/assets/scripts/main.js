9e393c4843619cf1848c31ad7ad62ff2ae0ba48e
Robert Katzki <katzki@ubilabs.net>
2013-07-12 10:08:12 +0200

    Remove commented code

--- www/assets/scripts/main.js
+++ www/assets/scripts/main.js
@@ -93,32 +93,3 @@ App.Main = Backbone.View.extend({
   }
 });
 new App.Main();
-
-
-// // // initial load of all todo items from the store
-// // hoodie.store.findAll('todo').then( function(todos) {
-// //   todos.sort( sortByCreatedAt ).forEach( addTodo )
-// // })
-
-// // // when a new todo gets stored, add it to the UI
-// // hoodie.store.on('add:todo', addTodo)
-// // // clear todo list when the get wiped from store
-// // hoodie.account.on('signout', clearTodos)
-
-// // // handle creating a new task
-// // $('#todoinput').on('keypress', function(event) {
-// //   if (event.keyCode == 13) { // ENTER
-// //     hoodie.store.add('todo', {title: event.target.value});
-// //     event.target.value = '';
-// //   }
-// // })
-
-// // function addTodo( todo ) {
-// //   $('#todolist').append('<li>'+todo.title+'</li>');
-// // }
-// // function clearTodos() {
-// //   $('#todolist').html('');
-// // }
-// // function sortByCreatedAt(a, b) {
-// //   return a.createdAt > b.createdAt
-// // }
